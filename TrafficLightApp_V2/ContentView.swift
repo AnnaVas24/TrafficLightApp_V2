4f7f06caf5b39dc5469b4df9a4eea00e778071db
Anna <anna.vasi4ko@icloud.com>
2022-02-15 14:36:32 +0300

    Initial Commit

---
+++ TrafficLightApp_V2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TrafficLightApp_V2
+//
+//  Created by Vasichko Anna on 15.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TrafficLightApp_V2/TrafficLightApp_V2App.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp_V2App.swift
+//  TrafficLightApp_V2
+//
+//  Created by Vasichko Anna on 15.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp_V2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
